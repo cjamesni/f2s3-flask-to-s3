a1712d69476d5f77439177dd45b1f12b42362104
cjamesni <23433005+cjamesni@users.noreply.github.com>
2021-08-22 22:21:50 +0100

    Update app.py, s3_functions.py, uploads, templates, index.html and collection.html

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
 from flask import Flask, render_template, request, redirect, send_file
-from s3_functions import upload_file, show_image
+from s3_functions import list_files, upload_file, show_image
 from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+BUCKET = "f2s3-media-data"
 
 @app.route("/")
 def home():
+    contents = list_files(BUCKET)
     return render_template('index.html')
+
+@app.route("/pics")
+def list():
+    contents = show_image(BUCKET)
+    return render_template('collection.html', contents=contents)
 
 @app.route("/upload", methods=['POST'])
 def upload():
@@ -14,10 +24,6 @@ def upload():
         f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
         upload_file(f"uploads/{f.filename}", BUCKET)
         return redirect("/")
-
-app = Flask(__name__)
-UPLOAD_FOLDER = "uploads"
-BUCKET = "f2s3-media-data"
 
 if __name__ == '__main__':
     app.run(debug=True)
