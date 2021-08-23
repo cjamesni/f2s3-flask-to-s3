23dad9f44f685a784fa45737d79164815c88cc9e
cjamesni <23433005+cjamesni@users.noreply.github.com>
2021-08-23 17:23:10 +0100

    Adding flask freeze config

---
+++ freeze.py
@@ -0,0 +1,11 @@
+# standard freeze script
+
+from flask_frozen import Freezer
+from app import app
+
+app.config['FREEZER_RELATIVE_URLS'] = True
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
